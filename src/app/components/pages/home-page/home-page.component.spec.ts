5ac78e21cdd9f2918962ce50d646fb154d4d91a8
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-05-14 12:21:45 +0200

    refactor: create home page component

---
+++ src/app/components/pages/home-page/home-page.component.spec.ts
@@ -0,0 +1,23 @@
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { HomePageComponent } from './home-page.component';
+
+describe('HomePageComponent', () => {
+  let component: HomePageComponent;
+  let fixture: ComponentFixture<HomePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomePageComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomePageComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
