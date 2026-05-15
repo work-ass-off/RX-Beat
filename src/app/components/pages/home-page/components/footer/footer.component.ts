d43b3fecfe3fa6a616275e2e62874fb404c6f9d4
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-05-15 11:15:24 +0200

    feat: implement footer comonent with content and links for home page

---
+++ src/app/components/pages/home-page/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'footer[app-home-footer]',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {}
