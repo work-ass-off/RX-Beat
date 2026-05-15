b1d548397232418683fdca3cfc41f6248a51d2da
Nikita <snitkonikita@gmail.com>
2026-05-15 14:50:41 +0200

    feat: add localization

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -2,10 +2,13 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { App } from '../../../app';
 import { SearchComponent } from '../search/search.component';
 import { ThemeButtonComponent } from '../theme-button/theme-button.component';
+import { LanguageDirective } from '../../../directives/language/language.directive';
+import { UpperCasePipe } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'header[app-header]',
-  imports: [SearchComponent, ThemeButtonComponent],
+  imports: [SearchComponent, ThemeButtonComponent, LanguageDirective, UpperCasePipe, TranslatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
