191aedb0108929fcef7853a238e2c72d7714bb6a
Anna Vasilevich <dzichonka@gmail.com>
2026-05-16 20:25:01 +0200

    refactor: make separate track card component

--- src/app/components/shared/jamendo/jamendo.component.ts
+++ src/app/components/shared/jamendo/jamendo.component.ts
@@ -1,23 +1,17 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { PlayerStoreService } from '../../../services/store/player-store/player-store.service';
-import type { JamendoTrack } from '../../../models/jamendo.model';
 import { JamendoTracksService } from '../../../services/jamendo/jamendo-tracks/jamendo-tracks.service';
 import { LoaderComponent } from '../loader/loader.component';
+import { TrackCardComponent } from '../track-card/track-card.component';
 
 @Component({
   selector: 'app-jamendo',
-  imports: [LoaderComponent],
+  imports: [LoaderComponent, TrackCardComponent],
   templateUrl: './jamendo.component.html',
   styleUrl: './jamendo.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class JamendoComponent {
   public jamendoTracksService = inject(JamendoTracksService);
-  private trackStoreService = inject(PlayerStoreService);
 
   tracks = this.jamendoTracksService.tracksResource;
-
-  onClick(track: JamendoTrack): void {
-    this.trackStoreService.setTrack(track);
-  }
 }
