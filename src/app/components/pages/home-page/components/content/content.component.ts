3eec0472f0533375316f9b8f91943a1c2b8c42f2
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-05-18 17:16:02 +0200

    feat: implement genre, album preview components and mock music collection

---
+++ src/app/components/pages/home-page/components/content/content.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MUSIC_COLLECTION_MOCK } from './music-collection.mock';
+import { GenreSectionComponent } from './genre-section/genre-section.component';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [GenreSectionComponent],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContentComponent {
+  readonly musicCollectionMock = input(MUSIC_COLLECTION_MOCK);
+
+  get genres() {
+    return Object.values(this.musicCollectionMock());
+  }
+
+}
