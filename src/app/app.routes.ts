4c761a9ca7973336208f0f6c89d7284bf0d4ca08
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-05-19 15:32:47 +0200

    feat: create not found page template, implement render via router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./components/pages/home-page/home-page.component').then((m) => m.HomePageComponent),
   },
-  { path: '**', redirectTo: '' },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/pages/not-found-page/not-found-page.component').then((m) => m.NotFoundPageComponent),
+  },
 ];
---
+++ src/app/components/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-page',
+  imports: [],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotFoundPageComponent {}
