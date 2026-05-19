82a9c7f5d633c056de08188e011c28914bd51938
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-05-19 16:19:51 +0200

    feat: create all, music, podcasts  pages, implement render via router inside home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,31 @@
 import type { Routes } from '@angular/router';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./components/pages/home-page/home-page.component').then((m) => m.HomePageComponent),
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    children: [
+        {
+        path: '',
+        redirectTo: 'all',
+        pathMatch: 'full'
+      },
+      {path: 'all', loadComponent: () => import('./components/pages/all-page/all-page.component').then((m) => m.AllPageComponent)}, 
+      {path: 'music', loadComponent: () => import('./components/pages/music-page/music-page.component').then((m) => m.MusicPageComponent)},
+      {path: 'podcasts', loadComponent: () => import('./components/pages/podcasts-page/podcasts-page.component').then((m) => m.PodcastsPageComponent)},
+    ],
+  },
+
+  {
+    path: 'login',
+    loadComponent: () => import('./components/pages/login-page/login-page.component').then((m) => m.LoginPageComponent),
+  },  
   {
     path: '**',
     loadComponent: () =>
