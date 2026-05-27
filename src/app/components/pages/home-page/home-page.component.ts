3f88c3ede72b3a10c3a4d0ee107b4940f0cfc597
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-05-27 12:06:51 +0200

    feat: add import for Content component

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -5,6 +5,7 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarWrapperComponent } from '../../shared/sidebar-wrapper/sidebar-wrapper.component';
 import { LeftHeaderComponent } from './components/side/left-side-content/left-header/left-header.component';
 import { LeftContentComponent } from './components/side/left-side-content/left-content/left-content.component';
+import { ContentComponent } from './components/content/content.component';
 
 @Component({
   selector: 'app-home-page',
