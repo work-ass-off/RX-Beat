6fee0c10eaeb25777f8000362929ccaf837e6c87
Anna Vasilevich <dzichonka@gmail.com>
2026-05-30 14:31:52 +0200

    feat: implement signup and signin forms for login gard

--- src/app/components/pages/api-page/api-page.component.ts
+++ src/app/components/pages/api-page/api-page.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { SignupFormComponent } from '../../shared/signup-form/signup-form.component';
-import { SigninFormComponent } from '../../shared/signin-form/signin-form.component';
+import { SignupFormComponent } from '../signup-page/signup-form/signup-form.component';
+import { SigninFormComponent } from '../login-page/signin-form/signin-form.component';
 
 @Component({
   selector: 'app-api-page',
   imports: [SignupFormComponent, SigninFormComponent],
   templateUrl: './api-page.component.html',
-  // styleUrl: './api-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'page',
