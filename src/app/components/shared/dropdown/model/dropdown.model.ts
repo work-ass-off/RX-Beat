503f7a4acce7106fcff08806658c2cf181c9c3e2
Mikita <93331548+Snitkon@users.noreply.github.com>
2026-06-01 15:48:51 +0200

    Create reusable sidebar component (Right) (#76)
    * feat: add right sidebar and add dropdown

---
+++ src/app/components/shared/dropdown/model/dropdown.model.ts
@@ -0,0 +1,12 @@
+// TODO: Type Dropdown
+export type Dropdown = {
+  btnText: string;
+  items: DropdownItem[];
+};
+
+// TODO: Type Item
+export type DropdownItem = {
+  id: number;
+  icon: string;
+  title: string;
+};
