fcd275536a516cec2d84713e2b8f15fd4d28e017
Mikita Snitko <snitkonikita@gmail.com>
2026-06-07 18:49:23 +0200

    feat: add basic actions for albums

--- src/app/store/albums/albums.actions.ts
+++ src/app/store/albums/albums.actions.ts
@@ -1,10 +1,25 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import type { JamendoAlbums } from '../../models/jamendo.model';
 
 export const AlbumsActions = createActionGroup({
   source: 'Albums',
   events: {
-    'Load Albumss': emptyProps(),
-    'Load Albumss Success': props<{ data: unknown }>(),
-    'Load Albumss Failure': props<{ error: unknown }>(),
+    // * ALBUMS
+
+    'Load Albums': emptyProps(),
+    'Load Albums Success': props<{ data: JamendoAlbums[] }>(),
+    'Load Albums Failure': props<{ error: string }>(),
+
+    // * ALBUMS/TRACKS
+
+    'Load Albums Tracks': emptyProps(),
+    'Load Albums Tracks Success': props<{ data: JamendoAlbums[] }>(),
+    'Load Albums Tracks Failure': props<{ error: string }>(),
+
+    // * ALBUMS/MUSICINFO
+
+    'Load Albums MusicInfo': emptyProps(),
+    'Load Albums MusicInfo Success': props<{ data: JamendoAlbums[] }>(),
+    'Load Albums MusicInfo Failure': props<{ error: string }>(),
   },
 });
