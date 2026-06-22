1d2338dcdb049565f10a974b6e799dc382a9e4af
Mikita Snitko <snitkonikita@gmail.com>
2026-06-22 20:34:59 +0200

    feat: add artist component

---
+++ src/app/components/shared/artist/artist.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import type { Artist } from '../../../models';
+
+@Component({
+  selector: 'app-artist',
+  imports: [],
+  templateUrl: './artist.component.html',
+  styleUrl: './artist.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ArtistComponent {
+  public artist = input.required<Artist>();
+}
