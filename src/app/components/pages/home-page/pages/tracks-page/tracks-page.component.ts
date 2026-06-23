c19174ec8094bcd7c6145d0cc5ffd42834cb35d4
Mikita Snitko <snitkonikita@gmail.com>
2026-06-23 18:52:18 +0200

    refactor: change structure tracks page

--- src/app/components/pages/home-page/pages/tracks-page/tracks-page.component.ts
+++ src/app/components/pages/home-page/pages/tracks-page/tracks-page.component.ts
@@ -1,17 +1,53 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ContentComponent } from '../../components/content/content.component';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { JamendoTracksService } from '../../../../../services/jamendo/jamendo-tracks/jamendo-tracks.service';
+import { TrackComponent } from '../../../../shared/track/track.component';
+import { LoadingService } from '../../../../../services/loading/loading.service';
+import { NotificationService } from '../../../../../services/notification/notification.service';
+import { map, switchMap, type Observable } from 'rxjs';
+import type { Track } from '../../../../../models';
+import { AsyncPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { JamendoAlbumsService } from '../../../../../services/jamendo/jamendo-albums/jamendo-albums.service';
+import { JamendoArtistsService } from '../../../../../services/jamendo/jamendo-artists/jamendo-artists.service';
+import { LoaderSpinnerComponent } from '../../../../shared/loader-spinner/loader-spinner.component';
 
 @Component({
   selector: 'app-tracks-page',
-  imports: [ContentComponent],
+  imports: [TrackComponent, AsyncPipe, LoaderSpinnerComponent],
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.scss',
+  host: {
+    class: 'tracks',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TracksPageComponent {
   private jamendoTracksService = inject(JamendoTracksService);
+  private jamendoAlbumsService = inject(JamendoAlbumsService);
+  private jamendoArtistsService = inject(JamendoArtistsService);
 
-  private tracksSignal = toSignal(this.jamendoTracksService.getTracks(), { initialValue: [] });
+  private loadingService = inject(LoadingService);
+  private route = inject(ActivatedRoute);
+
+  public notificationService = inject(NotificationService);
+
+  public loading = this.loadingService.isLoaderActive('tracks');
+
+  public tracks$: Observable<Track[]> = this.route.paramMap.pipe(
+    map((params) => ({
+      album: params.get('albumId'),
+      artist: params.get('artistId'),
+    })),
+    switchMap((params) => {
+      if (params.album) {
+        return this.jamendoAlbumsService.getAlbumWithTracks(params.album).pipe(map((album) => album?.tracks ?? []));
+      }
+      if (params.artist) {
+        return this.jamendoArtistsService
+          .getArtistWithTracks(params.artist)
+          .pipe(map((artist) => artist?.tracks ?? []));
+      }
+      return this.jamendoTracksService.getTracks();
+    }),
+  );
 }
