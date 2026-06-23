d2283631fd6669757c16ab1d830f9d50d41d6f88
Mikita Snitko <snitkonikita@gmail.com>
2026-06-23 18:53:47 +0200

    refactor: change structure albums page

--- src/app/components/pages/home-page/pages/albums-page/albums-page.component.ts
+++ src/app/components/pages/home-page/pages/albums-page/albums-page.component.ts
@@ -1,17 +1,30 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ContentComponent } from '../../components/content/content.component';
 import { JamendoAlbumsService } from '../../../../../services/jamendo/jamendo-albums/jamendo-albums.service';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { NotificationService } from '../../../../../services/notification/notification.service';
+import type { Observable } from 'rxjs';
+import type { Album } from '../../../../../models';
+import { AsyncPipe } from '@angular/common';
+import { LoadingService } from '../../../../../services/loading/loading.service';
+import { AlbumComponent } from '../../../../shared/album/album.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoaderSpinnerComponent } from '../../../../shared/loader-spinner/loader-spinner.component';
 
 @Component({
   selector: 'app-albums-page',
-  imports: [ContentComponent],
+  imports: [AlbumComponent, AsyncPipe, AlbumComponent, RouterLink, RouterOutlet, LoaderSpinnerComponent],
   templateUrl: './albums-page.component.html',
   styleUrl: './albums-page.component.scss',
+  host: {
+    class: 'albums',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlbumsPageComponent {
   private jamendoAlbumsService = inject(JamendoAlbumsService);
+  private loadingService = inject(LoadingService);
 
-  private albumsSignal = toSignal(this.jamendoAlbumsService.getAlbums(), { initialValue: [] });
+  public notificationService = inject(NotificationService);
+
+  public albums$: Observable<Album[]> = this.jamendoAlbumsService.getAlbums();
+  public loading = this.loadingService.isLoaderActive('albums');
 }
