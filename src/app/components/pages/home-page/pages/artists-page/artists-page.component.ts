bceb0fe0b6267d3a875f64bdfacbd906d60c48c7
Mikita Snitko <snitkonikita@gmail.com>
2026-06-23 18:53:07 +0200

    refactor: change structure artists page

--- src/app/components/pages/home-page/pages/artists-page/artists-page.component.ts
+++ src/app/components/pages/home-page/pages/artists-page/artists-page.component.ts
@@ -1,17 +1,30 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ContentComponent } from '../../components/content/content.component';
 import { JamendoArtistsService } from '../../../../../services/jamendo/jamendo-artists/jamendo-artists.service';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { LoadingService } from '../../../../../services/loading/loading.service';
+import { NotificationService } from '../../../../../services/notification/notification.service';
+import type { Observable } from 'rxjs';
+import type { Artist } from '../../../../../models';
+import { AsyncPipe } from '@angular/common';
+import { ArtistComponent } from '../../../../shared/artist/artist.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoaderSpinnerComponent } from '../../../../shared/loader-spinner/loader-spinner.component';
 
 @Component({
   selector: 'app-artists-page',
-  imports: [ContentComponent],
+  imports: [ArtistComponent, AsyncPipe, RouterLink, RouterOutlet, LoaderSpinnerComponent],
   templateUrl: './artists-page.component.html',
   styleUrl: './artists-page.component.scss',
+  host: {
+    class: 'artists',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArtistsPageComponent {
   private jamendoArtistsService = inject(JamendoArtistsService);
+  private loadingService = inject(LoadingService);
 
-  private artistsSignal = toSignal(this.jamendoArtistsService.getArtists(), { initialValue: [] });
+  public notificationService = inject(NotificationService);
+
+  public artists$: Observable<Artist[]> = this.jamendoArtistsService.getArtists();
+  public loading = this.loadingService.isLoaderActive('artists');
 }
