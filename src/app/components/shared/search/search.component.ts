d3c5613b85aaac1551c9bb09636abfbfc9d0c3b3
Mikita Snitko <snitkonikita@gmail.com>
2026-06-29 17:30:46 +0200

    feat: new implementation for search

--- src/app/components/shared/search/search.component.ts
+++ src/app/components/shared/search/search.component.ts
@@ -2,25 +2,61 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { JamendoTracksService } from '../../../services/jamendo/jamendo-tracks/jamendo-tracks.service';
 import { TranslatePipe } from '@ngx-translate/core';
+import { JamendoAutocompleteService } from '../../../services/jamendo/jamendo-autocomplete/jamendo-autocomplete.service';
+import { JamendoAlbumsService } from '../../../services/jamendo/jamendo-albums/jamendo-albums.service';
+import { AsyncPipe } from '@angular/common';
+import { JamendoArtistsService } from '../../../services/jamendo/jamendo-artists/jamendo-artists.service';
+import { RoutingService } from '../../../services/routing/routing.service';
 
 @Component({
   selector: 'app-search',
-  imports: [FormsModule, TranslatePipe],
+  imports: [FormsModule, TranslatePipe, AsyncPipe],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchComponent {
-  public jamendoTracksService = inject(JamendoTracksService);
+  private jamendoTracksService = inject(JamendoTracksService);
+  private jamendoArtistsService = inject(JamendoArtistsService);
+  private jamendoAlbumsService = inject(JamendoAlbumsService);
+  private jamendoAutocompleteService = inject(JamendoAutocompleteService);
+  private routingService = inject(RoutingService);
 
-  public onTypeSearch(track: string): void {
-    this.jamendoTracksService.isAutocompleteOpen.set(true);
-    this.jamendoTracksService.query.set(track);
+  public isAutocompleteOpen = this.jamendoAutocompleteService.isAutocompleteOpen.asReadonly();
+  public autocomplete$ = this.jamendoAutocompleteService.autocomplete$;
+  public query = this.jamendoAutocompleteService.queryData.asReadonly();
+  public getItem = this.jamendoAutocompleteService.getAutocompleteItem;
+
+  public isOpened(value: boolean): void {
+    this.jamendoAutocompleteService.isAutocompleteOpen.set(value);
   }
 
-  public onConfirmSearch(track: string): void {
-    this.jamendoTracksService.query.set(track);
-    this.jamendoTracksService.isAutocompleteOpen.set(false);
-    this.jamendoTracksService.tracksResource.reload();
+  public onTypeSearch(search: string): void {
+    this.jamendoAutocompleteService.isAutocompleteOpen.set(true);
+    this.jamendoAutocompleteService.queryData.set(search);
+  }
+
+  public onConfirmSearch(search: string): void {
+    this.jamendoAutocompleteService.queryData.set(search);
+    const currentPath = this.routingService.path();
+
+    switch (currentPath) {
+      case 'tracks':
+        // * Tracks Logic
+
+        this.jamendoTracksService.activeTracksSearch.set(search);
+        break;
+      case 'albums':
+        // * Albums Logic
+
+        this.jamendoAlbumsService.activeAlbumsSearch.set(search);
+        break;
+      case 'artists':
+        // * Artist Logic
+
+        this.jamendoArtistsService.activeArtistsSearch.set(search);
+        break;
+    }
+    this.jamendoAutocompleteService.isAutocompleteOpen.set(false);
   }
 }
