fac574bc5004a0c0eeaead07278d8ff20757fe39
Mikita Snitko <snitkonikita@gmail.com>
2026-06-29 17:25:54 +0200

    feat: new implementation for albums and artists service for search logic

--- src/app/services/jamendo/jamendo-albums/jamendo-albums.service.ts
+++ src/app/services/jamendo/jamendo-albums/jamendo-albums.service.ts
@@ -1,9 +1,10 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { JamendoService } from '../jamendo.service';
-import { catchError, EMPTY, map, type Observable } from 'rxjs';
+import { catchError, EMPTY, map, switchMap, type Observable } from 'rxjs';
 import type { Album, JamendoAlbumsResponse } from '../../../models/';
 import { NotificationService } from '../../notification/notification.service';
 import type { HttpErrorResponse } from '@angular/common/http';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,24 @@ import type { HttpErrorResponse } from '@angular/common/http';
 export class JamendoAlbumsService {
   private _jamendoService = inject(JamendoService);
   private _notificationService = inject(NotificationService);
+  public activeAlbumsSearch = signal<string>('');
+
+  public albums$ = toObservable(this.activeAlbumsSearch).pipe(
+    switchMap((searchQuery) => {
+      const params: Record<string, unknown> = { limit: 30 };
+      const namesearch = searchQuery.trim();
+      if (namesearch) {
+        params['namesearch'] = namesearch;
+      }
+      return this._jamendoService.getWithHttpClient<JamendoAlbumsResponse>('albums', params, 'albums').pipe(
+        map((response) => response.results),
+        catchError((error: HttpErrorResponse) => {
+          this._notificationService.show(error.message || 'Something went wrong');
+          return EMPTY;
+        }),
+      );
+    }),
+  );
 
   public getAlbums(): Observable<Album[]> {
     return this._jamendoService.getWithHttpClient<JamendoAlbumsResponse>('albums', { limit: 30 }, 'albums').pipe(
