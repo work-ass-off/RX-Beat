764ea7444086bb5ce3f22de2bde33e21b3c4da8a
Mikita Snitko <snitkonikita@gmail.com>
2026-07-03 16:11:29 +0200

    feat: add auth check on tracks page

--- src/app/components/pages/home-page/pages/tracks-page/tracks-page.component.ts
+++ src/app/components/pages/home-page/pages/tracks-page/tracks-page.component.ts
@@ -6,12 +6,13 @@ import { map, type Observable, switchMap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { LoaderSpinnerComponent } from '../../../../shared/loader-spinner/loader-spinner.component';
 import { JamendoAbstractService } from '../../../../../services/jamendo/jamendo-abstract/jamendo-abstract.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import type { Track } from '../../../../../models';
+import { AuthService } from '../../../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-tracks-page',
-  imports: [TrackComponent, AsyncPipe, LoaderSpinnerComponent],
+  imports: [TrackComponent, AsyncPipe, LoaderSpinnerComponent, RouterLink],
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.scss',
   host: {
@@ -23,15 +24,17 @@ export class TracksPageComponent {
   private _jamendoService = inject(JamendoAbstractService);
   private _loadingService = inject(LoadingService);
   private _activatedRoute = inject(ActivatedRoute);
+  private _authService = inject(AuthService);
 
   public notificationService = inject(NotificationService);
+  public isAuthorized = this._authService.isLoggedIn;
   public loading = this._loadingService.isLoaderActive('tracks');
 
   public tracks$: Observable<Track[]> = this._activatedRoute.paramMap.pipe(
     map((params) => params.get('id')),
     switchMap((id) => {
       if (id) {
-        return this._jamendoService.getDataById(id).pipe(map((result) => result.tracks ?? []));
+        return this._jamendoService.getDataById(id).pipe(map((result) => result?.tracks ?? []));
       }
       return this._jamendoService.data$;
     }),
