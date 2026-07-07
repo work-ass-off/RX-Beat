cc88e1ec69accbd18c74fa521ab3c9b4ddddd03c
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-07-07 16:31:22 +0200

    feat: implement test for checking defulat values for player service

--- src/app/services/store/player-store/player-store.service.spec.ts
+++ src/app/services/store/player-store/player-store.service.spec.ts
@@ -13,4 +13,40 @@ describe('PlayerStoreService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('isInitialLoading should be true by default', () => {
+    expect(service.isInitialLoading()).toBeTruthy()
+  })
+
+  it('currentTrack should be null by default', () => {
+    expect(service.currentTrack()).toBeNull()
+  })
+
+  it('audio should be null by default', () => {
+    expect(service.audio()).toBeNull()
+  });
+
+  it('isPlaying should be false by default', () => {
+    expect(service.isPlaying()).toBeFalsy()
+  });
+
+  it('isLidkedTrack should be false by default', () => {
+    expect(service.isLikedTrack()).toBeFalsy()
+  })
+
+  it('volume should be 1 by default', () => {
+    expect(service.volume()).toBe(1)
+  })
+
+  it('trackCurrentTime should be 0 by default', () => {
+    expect(service.trackCurrentTime()).toBe(0)
+  });
+
+  it('queueOfPlayedTracks should be empty by default', () => {
+    expect(service.queueOfPlayedTracks()).toEqual([] )
+  });
+
+  it('queueOfPlayedTracks should be as active queue by default', () => {
+    expect(service.activeQueue()).toBe('queueOfPlayedTracks')
+  });
 });
