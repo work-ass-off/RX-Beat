6af453e4f915dad32b5cc1b51b7ab82f61a071ba
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-07-10 12:32:55 +0200

    feat: update tests for about-page

--- src/app/components/pages/about-page/about-page.component.spec.ts
+++ src/app/components/pages/about-page/about-page.component.spec.ts
@@ -1,15 +1,15 @@
 import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { AboutPageComponent } from './about-page.component';
-
 describe('AboutPageComponent', () => {
   let component: AboutPageComponent;
   let fixture: ComponentFixture<AboutPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutPageComponent],
+      imports: [AboutPageComponent, TranslateModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutPageComponent);
@@ -18,24 +18,40 @@ describe('AboutPageComponent', () => {
     await fixture.whenStable();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a title', () => {
+
+  it('should have an image', () => {
     const element = fixture.nativeElement;
-    console.log(element);
+    const image = element.querySelector('img');
+    expect(image).toBeTruthy();
+    expect(image.src).toContain('assets/gif/black-cat.gif');
+
+  });
+
+  it('title should have a text', () => {
+    const element = fixture.nativeElement;
     const title = element.querySelector('h2');
     expect(title).toBeTruthy();
-    expect(title.textContent).toContain('About');
+    expect(title.textContent).toBeDefined();
   });
 
   it('should have a description', () => {
     const element = fixture.nativeElement;
     const description = element.querySelector('p');
     expect(description).toBeTruthy();
-    expect(description.textContent).toContain(
-      'WorkAssOff is an application that allows you to find information about artists, albums, tracks, and genres.',
-    );
+    expect(description.textContent).toBeDefined();
+  });
+
+  it('should have 3 links to the GitHub repository', () => {
+    const element = fixture.nativeElement;
+    const link = element.querySelector('a');
+    const allLinks = element.querySelectorAll('a');
+    expect(link).toBeTruthy();
+    expect(link.href).toContain('github.com');
+    expect(allLinks.length).toBe(3);
   });
 });
