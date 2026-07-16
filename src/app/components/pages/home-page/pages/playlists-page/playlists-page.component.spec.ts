19e9cd89c3cd605b180690f8c087171e5d54a800
Anna Vasilevich <dzichonka@gmail.com>
2026-07-16 12:23:58 +0100

    feat: delete unuseable tests, add playlist-page.spec, tracks-page.spec, add ng test to husky pre comit

--- src/app/components/pages/home-page/pages/playlists-page/playlists-page.component.spec.ts
+++ src/app/components/pages/home-page/pages/playlists-page/playlists-page.component.spec.ts
@@ -1,23 +1,82 @@
-import type { ComponentFixture } from '@angular/core/testing';
-import { TestBed } from '@angular/core/testing';
-
+import { signal } from '@angular/core';
+import { type ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { LoadingService } from '../../../../../services/loading/loading.service';
+import { RxBeatApiService } from '../../../../../services/rx-beat-api/rx-beat-api.service';
 import { PlaylistsPageComponent } from './playlists-page.component';
 
 describe('PlaylistsPageComponent', () => {
-  let component: PlaylistsPageComponent;
   let fixture: ComponentFixture<PlaylistsPageComponent>;
+  let component: PlaylistsPageComponent;
+
+  const playlist = {
+    id: 'playlist-1',
+    name: 'My playlist',
+    tracks: ['track-1', 'track-2'],
+    userId: 'user-1',
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  };
+
+  const rxBeatApiServiceMock = {
+    playlists$: of([]),
+  };
+
+  const loadingServiceMock = {
+    isLoaderActive: vi.fn().mockReturnValue(signal(false)),
+  };
+
+  const routerMock = {
+    navigate: vi.fn(),
+  };
+
+  const activatedRouteMock = {};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PlaylistsPageComponent],
+      providers: [
+        {
+          provide: RxBeatApiService,
+          useValue: rxBeatApiServiceMock,
+        },
+        {
+          provide: LoadingService,
+          useValue: loadingServiceMock,
+        },
+        {
+          provide: Router,
+          useValue: routerMock,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: activatedRouteMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PlaylistsPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create playlist page', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set active tracks and navigate to playlist', () => {
+    component.navigateToPlaylist(playlist);
+
+    expect(component.activeTracks()).toEqual(['track-1', 'track-2']);
+
+    expect(routerMock.navigate).toHaveBeenCalledWith(['playlist-1'], {
+      relativeTo: activatedRouteMock,
+    });
+  });
+
+  it('should navigate to playlist', () => {
+    component.navigateToPlaylist(playlist);
+
+    expect(routerMock.navigate).toHaveBeenCalled();
   });
 });
